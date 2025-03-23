0a019d1feb48a185fa653b2a2e939c72f9152e98
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-03-23 15:17:21 +0600

    map(),forEach,filter,find

--- script/jsRecape.js
+++ script/jsRecape.js
@@ -1,52 +1,52 @@
-// //  variable-----
-// let name = "hero";
-// name = 'programming hero';
+//  variable-----
+let name = "hero";
+name = 'programming hero';
 // console.log(name);
-// const country = 'bangladesh';
+const country = 'bangladesh';
 
-// // condition-----
-// if (name === 'programming hero' && country === 'bangladesh') {
-//     console.log('sorto thik ase');
-// }
-// else {
-//     console.log("sorto thik nai");
-// }
-// // array---------
-// const friends = ['salam', 'kalam', 'rohim', 'rolamdo', 'rojina']
+// condition-----
+if (name === 'programming hero' && country === 'bangladesh') {
+    // console.log('sorto thik ase');
+}
+else {
+    // console.log("sorto thik nai");
+}
+// array---------
+const friends = ['salam', 'kalam', 'rohim', 'rolamdo', 'rojina']
 
-// // friends.push('josona')
-// // friends.pop()
-// // friends.unshift('natural');
-// // friends.shift()
-// // console.table(friends)
-// // console.log(friends[0]);
-// // object-------
-// const person = {
-//     name: 'dolon',
-//     age: 10,
-//     friend: friends,
-//     status: 'no status'
-// }
-// name = 'daliya';
+// friends.push('josona')
+// friends.pop()
+// friends.unshift('natural');
+// friends.shift()
+// console.table(friends)
+// console.log(friends[0]);
+// object-------
+const person = {
+    name: 'dolon',
+    age: 10,
+    friend: friends,
+    status: 'no status'
+}
+name = 'daliya';
 // console.dir(person);
 // console.log(person.friend[1]);
-// // function--------
-// function sum(a, b) {
-//     const result = a + b;
-//     console.log(result);
-// }
-// sum(3, 7)
-// function mul(c, d) {
-//     console.log(...arguments);   //function ar argument chack.
-//     const result = c * d;
-//     return result;
-// }
-// const output = mul(2, 6)
+// function--------
+function sum(a, b) {
+    const result = a + b;
+    // console.log(result);
+}
+sum(3, 7)
+function mul(c, d) {
+    // console.log(...arguments);   //function ar argument chack.
+    const result = c * d;
+    return result;
+}
+const output = mul(2, 6)
 // console.log(output);
 // console.log(mul.length);   //function ar length chack.
-// // loop-----
-// for(let i= 0; i<10; i++){
-// const element = i;
+// loop-----
+for(let i= 0; i<10; i++){
+const element = i;
 // console.log(element);
-// }
+}
 
