6a9a19a0d22acc217aba1c1b321141904f288a0d
Srabonti721 <somayaaktarshabonti721@gmail.com>
2025-03-23 06:23:03 +0600

    js complete reacpe

---
+++ script/jsRecape.js
@@ -0,0 +1,47 @@
+//  variable-----
+let name = "hero";
+name = 'programming hero';
+console.log(name);
+const country = 'bangladesh';
+
+// condition-----
+if (name === 'programming hero' && country === 'bangladesh') {
+    console.log('sorto thik ase');
+}
+else {
+    console.log("sorto thik nai");
+}
+// array---------
+const friends = ['salam', 'kalam', 'rohim', 'rolamdo', 'rojina']
+
+// friends.push('josona')
+// friends.pop()
+// friends.unshift('natural');
+// friends.shift()
+// console.table(friends)
+// console.log(friends[0]);
+// object-------
+const person = {
+    name: 'dolon',
+    age: 10,
+    friend: friends,
+    status: 'no status'
+}
+console.dir(person);
+console.log(person.friend[1]);
+// function--------
+function sum(a, b) {
+    const result = a + b;
+    console.log(result);
+}
+sum(3, 7)
+function mul(c, d) {
+    console.log(...arguments);   //function ar argument chack.
+    const result = c * d;
+    return result;
+}
+const output = mul(2, 6)
+console.log(output);
+console.log(mul.length);   //function ar length chack.
+
+
